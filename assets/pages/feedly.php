6961bffeebb125758aca986b66cad390e6a761b7
Die <48467821+RohanPunjani@users.noreply.github.com>
2019-10-01 21:46:50 +0530

    Add files via upload

---
+++ assets/pages/feedly.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <title>Feedly</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="feedly.css">
+</head> 
+<body class="bg-light">
+    
+    <nav class="navbar navbar-fixed">
+        <!-- Brand -->
+        <a class="navbar-brand" href="#">feedly</a>
+
+        <form action="" class="search-form">
+            <div class="form-group has-feedback">
+                <label for="search" class="sr-only">Search</label>
+                <input type="text" class="form-control" name="search" id="search" placeholder="search">
+                <span class="glyphicon glyphicon-search form-control-feedback"></span>
+            </div>
+        </form>
+        <a href="#" class="user-profile">
+            <i class="fa fa-user"></i>
+        </a>
+    </nav> 
+    <div class="container">
+        <div class="row">
+            <div class="col-md-9 content-displayer">
+                hello
+            </div>
+            <div class="col-md-9 content-displayer">
+                hello
+            </div>
+            <div class="col-md-9 content-displayer">
+                hello
+            </div>
+            <div class="col-md-3 fixed-sidebar">
+                Hello
+            </div>
+        </div>
+    </div>
+</body>
+</html>
